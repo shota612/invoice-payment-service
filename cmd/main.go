e467c47339d21ad34d6cc2a202a50a518a0b92c8
Shota Nakano <shotanakano62@gmail.com>
2024-07-23 13:54:57 +0900

    Add router to create a invoice and main file

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,34 @@
-package cmd
+package main
+
+import (
+	"github.com/shota612/invoice-payment-service/server/api"
+	"github.com/shota612/invoice-payment-service/server/controllers"
+	"github.com/shota612/invoice-payment-service/server/models"
+	"github.com/shota612/invoice-payment-service/server/repository"
+	"github.com/shota612/invoice-payment-service/server/usecase"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func main() {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	err = db.AutoMigrate(&models.Company{}, &models.User{}, &models.Client{}, &models.ClientBankAccount{}, &models.Invoice{})
+	if err != nil {
+		return
+	}
+
+	invoiceRepo := repository.NewInvoiceRepository(db)
+	invoiceUsecase := usecase.NewInvoiceUsecase(invoiceRepo)
+	invoiceController := controllers.NewInvoiceController(invoiceUsecase)
+
+	r := api.SetupRouter(invoiceController)
+
+	err = r.Run(":8080")
+	if err != nil {
+		return
+	}
+}
--- server/api/router.go
+++ server/api/router.go
@@ -1,1 +1,15 @@
 package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/shota612/invoice-payment-service/server/controllers"
+)
+
+func SetupRouter(invoiceController *controllers.InvoiceController) *gin.Engine {
+	router := gin.Default()
+	api := router.Group("/api")
+	{
+		api.POST("/invoices", invoiceController.CreateInvoice)
+	}
+	return router
+}
